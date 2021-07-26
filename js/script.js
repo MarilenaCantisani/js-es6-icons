6f2613007e2167e7026b0e11376fddcc469f4ba2
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-26 17:19:51 +0200

    + Added icon to all card

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ Milestone 3
 Creiamo una select con i tipi di icone e usiamola per filtrare le icone*/
 
 
+const displayCard = document.querySelector("#icons-section > .row");
+
+let iconsTemplate = "";
+
+icons.forEach((icon) => {
+    iconsTemplate += `
+    <div class="card col col-md-2 shadow-sm m-3">
+        <div class="card-body text-center">
+            <i class="${icon.family} ${icon.prefix}${icon.name} ${icon.prefix}2x"></i>
+            <h2 class="h6 text-uppercase">${icon.name.toUpperCase()}</h2>
+        </div>
+    </div>`;
+
+});
+
+console.log(iconsTemplate);
+
+displayCard.innerHTML = iconsTemplate;
